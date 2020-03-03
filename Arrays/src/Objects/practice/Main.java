24cf3b263c2fc307d6f97fb195eb6a5a2786c10f
MiguelJibaja <miguelangeljibaja@gmail.com>
2020-03-03 09:31:40 -0500

    Se agregó el challenge de arrays donde se ordene un arreglo en orden descendente

--- Arrays/src/Objects/practice/Main.java
+++ Arrays/src/Objects/practice/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-	    int [] myIntegers = getIntegers(5);
+	    int [] myIntegers = sortArray(getIntegers(5));
 	    for (int i=0;i<myIntegers.length;i++){
             System.out.println("Elemento #" + i + " tiene el valor: " + myIntegers[i]);
         }
@@ -31,5 +31,26 @@ public class Main {
             sum+= array[i];
         }
         return (double)sum/(double)array.length;
+     }
+
+     public static int[] sortArray(int[] array){
+        int[] orderArray = new int[array.length];
+        for (int i=0;i<array.length;i++){
+            orderArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i=0;i<orderArray.length-1;i++){
+                if(orderArray[i] < orderArray[i+1]) {
+                    temp = orderArray[i];
+                    orderArray[i] = orderArray[i+1];
+                    orderArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+         }
+        return orderArray;
      }
 }
