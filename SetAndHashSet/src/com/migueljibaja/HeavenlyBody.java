2d5d64d4d0c83f2e7799db3e7e250cf4ac633fa3
Miguel Jibaja <u011075@gmail.com>
2020-06-05 22:42:37 -0500

    Empezamos ejemplo de HashSet

---
+++ SetAndHashSet/src/com/migueljibaja/HeavenlyBody.java
@@ -0,0 +1,32 @@
+package com.migueljibaja;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(this.satellites);
+    }
+}
