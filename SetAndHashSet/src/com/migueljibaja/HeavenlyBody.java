7adde6f5e7c6ebbcc93a8e1a84951300eb3aa415
Miguel Jibaja <u011075@gmail.com>
2020-06-07 02:14:06 -0500

    Terminamos ejemplo de HashSet HeavenlyBody

--- SetAndHashSet/src/com/migueljibaja/HeavenlyBody.java
+++ SetAndHashSet/src/com/migueljibaja/HeavenlyBody.java
@@ -29,4 +29,25 @@ public class HeavenlyBody {
     public Set<HeavenlyBody> getSatellites() {
         return new HashSet<>(this.satellites);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        System.out.println("obj.getClass() is " + obj.getClass());
+        System.out.println("this.getClass() is " + this.getClass());
+        if ((obj == null) || (obj.getClass() != this.getClass())){
+            return false;
+        }
+
+        String objname = ((HeavenlyBody) obj).getName();
+        return this.name.equals(objname);
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println("hashcode called");
+         return this.name.hashCode() +57;
+    }
 }
