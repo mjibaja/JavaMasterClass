3d65ba4f8168871de1424dfe4c25e364e1f7a29b
Miguel Jibaja <u011075@gmail.com>
2020-06-09 22:46:59 -0500

    Se agrego Challenge de Hash usando como llave una clase Key

--- SetAndHashSet/src/com/migueljibaja/HeavenlyBody.java
+++ SetAndHashSet/src/com/migueljibaja/HeavenlyBody.java
@@ -1,53 +1,108 @@
 package com.migueljibaja;
 
+import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
-public class HeavenlyBody {
-    private final String name;
+public abstract class HeavenlyBody {
+    private final Key key;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
 
-    public String getName() {
-        return name;
+    public enum BodyTypes {
+        STAR,
+        PLANET,
+        DWARF_PLANET,
+        MOON,
+        COMET,
+        ASTEROID
+    }
+
+    public Key getKey() {
+        return key;
     }
 
     public double getOrbitalPeriod() {
         return orbitalPeriod;
     }
 
-    public HeavenlyBody(String name, double orbitalPeriod) {
-        this.name = name;
+    public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
+        this.key = new Key(name,bodyType);
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
+
     }
 
-    public boolean addMoon(HeavenlyBody moon) {
-        return this.satellites.add(moon);
+    public boolean addSatellite(HeavenlyBody moon){
+       return this.satellites.add(moon);
     }
 
-    public Set<HeavenlyBody> getSatellites() {
+    public Set<HeavenlyBody> getSatellites()      {
         return new HashSet<>(this.satellites);
     }
 
+
     @Override
-    public boolean equals(Object obj) {
+    public final boolean equals(Object obj) {
         if (this == obj){
             return true;
         }
-        System.out.println("obj.getClass() is " + obj.getClass());
-        System.out.println("this.getClass() is " + this.getClass());
-        if ((obj == null) || (obj.getClass() != this.getClass())){
-            return false;
-        }
 
-        String objname = ((HeavenlyBody) obj).getName();
-        return this.name.equals(objname);
+        if (obj instanceof HeavenlyBody) {
+            HeavenlyBody theObject = (HeavenlyBody) obj;
+            return this.key.equals(theObject.getKey());
+        }
+        return false;
     }
 
     @Override
     public int hashCode() {
-        System.out.println("hashcode called");
-         return this.name.hashCode() +57;
+       // System.out.println("hashcode called");
+         return this.key.hashCode();
+    }
+
+    public static Key makeKey(String name, BodyTypes bodyType) {
+        return new Key(name, bodyType);
+    }
+    @Override
+    public String toString() {
+        return this.key.name + ": " + this.key.bodyType + ", " + this.orbitalPeriod;
+    }
+
+    public static final class Key {
+        private String name;
+        private BodyTypes bodyType;
+
+        public Key(String name, BodyTypes bodyType) {
+            this.name = name;
+            this.bodyType = bodyType;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyType() {
+            return bodyType;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Key key = (Key) obj;
+            if (this.name.equals(key.getName())){
+                return (this.bodyType == key.getBodyType());
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + ": " + this.bodyType;
+        }
     }
 }
